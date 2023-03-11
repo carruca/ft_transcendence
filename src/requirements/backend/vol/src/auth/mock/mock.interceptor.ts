0ee169c79c16acf132309bacaa3a702585bd064e
Adorna-R <65570450+Adorna-R@users.noreply.github.com>
2023-03-12 00:04:35 +0100

    Set Mock auth method cookies secure dinamically

--- src/requirements/backend/vol/src/auth/mock/mock.interceptor.ts
+++ src/requirements/backend/vol/src/auth/mock/mock.interceptor.ts
@@ -3,14 +3,45 @@ import { MockService } from './mock.service';
 
 @Injectable()
 export class MockCallbackInterceptor implements NestInterceptor {
+
   async intercept(context: ExecutionContext, next: CallHandler): Promise<any> {  // TODO: Typing
     const req = context.switchToHttp().getRequest();
     const res = context.switchToHttp().getResponse();
+    const { secure } = req;
     if (process.env.NEST_AUTH_MOCK === 'true' && req.query.mock === 'true') {
       const data = await new MockService().login(req.query.code);
-      res.cookie('token', data.access_token, { httpOnly: false, signed: true, sameSite: true ? 'none' : 'lax', maxAge: 3600000, secure: true });
-      res.cookie('refresh_token', data.refresh_token, { httpOnly: false, signed: true, sameSite: true ? 'none' : 'lax', maxAge: 3600000, secure: true });
-      res.cookie('auth_method', 'mock', { httpOnly: false, signed: true, sameSite: true ? 'none' : 'lax', maxAge: 3600000, secure: true });
+      res.cookie(
+        'token',
+        data.access_token,
+        {
+          httpOnly: false,
+          signed: true,
+          sameSite: secure ? 'none' : 'lax',
+          maxAge: 3600000,
+          secure
+        }
+      );
+      res.cookie(
+        'refresh_token',
+        data.refresh_token,
+        {
+          httpOnly: false,
+          signed: true,
+          sameSite: secure ? 'none' : 'lax',
+          maxAge: 3600000,
+          secure
+        }
+      );
+      res.cookie(
+        'auth_method',
+        'mock',
+        {
+          httpOnly: false,
+          signed: true,
+          sameSite: secure ? 'none' : 'lax',
+          maxAge: 3600000,
+          secure
+        });
       return data;
     }
     return next.handle();
