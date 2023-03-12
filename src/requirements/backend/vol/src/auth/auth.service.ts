99d42a6b438ffef523f098dd884d20aaa24b0761
Adorna-R <65570450+Adorna-R@users.noreply.github.com>
2023-03-12 09:35:42 +0100

    Add mock auth logic

--- src/requirements/backend/vol/src/auth/auth.service.ts
+++ src/requirements/backend/vol/src/auth/auth.service.ts
@@ -17,8 +17,7 @@ export class AuthService {
 
   async getUser(authMethod: string, token: string): Promise<any> {
     try {
-      const auth = new this.authMethods[authMethod]();
-      const data = await auth.getUser(token);
+      const data = new this.authMethods[authMethod]().getUser(token);
       return data;
     } catch (error) {
       if (error instanceof TypeError) {
--- src/requirements/backend/vol/src/auth/mock/mock.service.ts
+++ src/requirements/backend/vol/src/auth/mock/mock.service.ts
@@ -4,16 +4,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MockService {
 
-  async login(_code: string): Promise<any> {
+  async login(code: string): Promise<any> {
     try {
-      // TODO: Implement mock login
-      /**
-       * Callable by /auth/callback?mock=true&code=..., called by a decorator
-       * This receives a 42 intra user's id and returns a mock
-       * token as if it was the legit user's token
-       */
       return {
-        access_token: 'mock_access_token',
+        access_token: code,
         refresh_token: 'mock_refresh_token'
       }
     } catch (error) {
@@ -21,15 +15,42 @@ export class MockService {
     }
   }
 
-  async getUser(_token: string): Promise<any> {
+  async intraBearer(): Promise<any> {
+    const response = await fetch(`${process.env.NEST_INTRA_API_URL}/oauth/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: process.env.NEST_INTRA_UID,
+        client_secret: process.env.NEST_INTRA_SECRET,
+        grant_type: 'client_credentials',
+        scope: 'public',
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  }
+
+  async getUser(token: string): Promise<any> {
     try {
-      // TODO: Implement mock getUser
-      /**
-       * This should return the user's data that is being mocked
-       */
-      return 'mock_user'
+      const { access_token } = await this.intraBearer();
+      const response = await fetch(`${process.env.NEST_INTRA_API_URL}/v2/users/${token}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
     } catch (error) {
       console.error(error);
+      throw new Error(error);
     }
   }
 }
