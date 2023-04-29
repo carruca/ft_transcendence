7a0b81f719ed86f5a92a839b19e5f149128ea89f
Miguel Ángel Adorna Ruiz <miguelangel.adornaruiz@telefonica.com>
2023-04-29 20:40:38 +0200

    fix: circular object reference (#11)
    * fix: circular structure response
    
    * Add .gitignore

--- src/requirements/backend/vol/src/auth/auth.controller.ts
+++ src/requirements/backend/vol/src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ export class AuthController {
   @ApiOperation({ description: 'Returns if the user is logged in or not, using signedCookies.auth_method and signedCookies.token' })
   @ApiResponse({ status: 200, description: 'User is logged in' })
   @ApiResponse({ status: 401, description: 'User is not logged in' })
-  async getAuth(@Req() req: Request, @Res({passthrough: true}) res: Response): Promise<Response> {
+  async getAuth(@Req() req: Request, @Res({passthrough: true}) res: Response): Promise<Object> {
     try {
       const data = await this.authService.getUser(
         req.signedCookies.auth_method,
@@ -21,10 +21,10 @@ export class AuthController {
         req.signedCookies.refresh_token
       );
       if (data) {
-        return res.status(HttpStatus.OK).json({
+        return {
           status: 'OK',
           message: 'User is logged in',
-        });
+        };
       }
     } catch (error) {
       console.error(error);
