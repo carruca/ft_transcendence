8ef9b0697515ba24d51ab23ddd0282a713397174
rnavarre at home <rnavarre@student.42madrid.com>
2023-11-27 13:02:29 +0100

    Fix: EventUser to aggregate user information for an event

--- src/requirements/frontend/vol/src/services/model/event.ts
+++ src/requirements/frontend/vol/src/services/model/event.ts
@@ -10,13 +10,16 @@ import {
   User,
 } from '.';
 
+interface EventUser {
+  id: string,
+  nick: string,
+}
+
 export class Event {
   public readonly id: string;
   public readonly type: EventTypeEnum;
-  public readonly sourceId: string;
-  public readonly targetId?: string;
-  public readonly sourceNickname: string;
-  public readonly targetNickname?: string;
+  public readonly source: EventUser;
+  public readonly target?: EventUser;
   public value: string;
   public readonly timestamp: Date;
   public edited: boolean;
@@ -24,10 +27,10 @@ export class Event {
   constructor (eventPayload: EventPayload) {
     this.id = eventPayload.id;
     this.type = eventPayload.type;
-    this.sourceId = eventPayload.sourceId;
-    this.targetId = eventPayload.targetId;
-    this.sourceNickname = eventPayload.sourceNickname;
-    this.targetNickname = eventPayload.targetNickname;
+    this.source.id = eventPayload.sourceId;
+    this.target.id = eventPayload.targetId;
+    this.source.name = eventPayload.sourceNickname;
+    this.target.name = eventPayload.targetNickname;
     this.value = eventPayload.value || '';
     this.timestamp = eventPayload.timestamp ? new Date(eventPayload.timestamp) : new Date();
     this.edited = eventPayload.edited;
