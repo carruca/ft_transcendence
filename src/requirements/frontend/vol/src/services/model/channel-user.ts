09504227dc724c67b9deb1b81c4b91af3d9f6673
rnavarre at home <rnavarre@student.42madrid.com>
2023-11-28 23:00:13 +0100

    Fix: merge conflicts

--- src/requirements/frontend/vol/src/services/model/channel-user.ts
+++ src/requirements/frontend/vol/src/services/model/channel-user.ts
@@ -42,10 +42,6 @@ export class ChannelUser {
         return this.user.siteRole;
     }
 
-    set siteRole(value: UserSiteRoleEnum) {
-        this.user.siteRole = value;
-    }
-
     get status(): UserStatusEnum {
         return this.user.status;
     }
--- src/requirements/frontend/vol/src/services/model/index.ts
+++ src/requirements/frontend/vol/src/services/model/index.ts
@@ -2,4 +2,3 @@ export * from './user';
 export * from './event';
 export * from './channel';
 export * from './channel-user';
-export * from './chat-event';
