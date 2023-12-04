ec4304538a0b7038943a5e9916a47e2eb4928fec
Tomas Sierra paradinas <tsierra-@student.42madrid.com>
2023-12-04 12:54:24 +0100

    silent warning in frontend console

--- srcs/requirements/frontend/vol/src/main.ts
+++ srcs/requirements/frontend/vol/src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import "./services/chat-client";
 
 const app = createApp(App);
+app.config.warnHandler = function(msg, vm, trace) { return null }
 
 app.use(router);
 
